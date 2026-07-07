f444457a40aee952af945596ce84c9b2cc123cbe
manasamurakonda <murakondamanasa@gmail.com>
2026-07-07 14:46:56 +0530

    revising linkedlists

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int main()
-{
-    cout<<"hey manasa"<<end;
+class Node{
+    public: 
+        int data;
+        Node *next;
+        Node(int d)
+        {
+            data = d;
+            next = NULL;
+        }
+};
+int main(){
+    vector<int> arr = {1,2,3,4,5,6,7,8,9,10};
+    Node *head = NULL,*temp = NULL;
+    for(int i=0;i<arr.size();i++)
+    {
+        if(head == NULL)
+        {
+            head = new Node(arr[i]);
+            temp = head;
+        }
+        else{
+            temp->next = new Node(arr[i]);
+            temp = temp->next;
+        }
+    }
+    cout<<"the linked list is: "<<endl;
+    temp = head;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
     return 0;
 }
